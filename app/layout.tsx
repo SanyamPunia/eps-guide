0a31032f8f3d13d2eb22bc6db0998d413a79cfaa
SanyamPunia <lewarends@gmail.com>
2025-10-27 01:13:08 +0530

    init layout + finalise content

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,11 @@ export const metadata: Metadata = {
   title: "EPS Guide",
   description:
     "A focused practical language and standard for rendering interfaces from permissions with deterministic rules small surface area and strong ergonomics",
+  openGraph: {
+    title: "EPS Guide",
+    description:
+      "A focused practical language and standard for rendering interfaces from permissions with deterministic rules small surface area and strong ergonomics",
+  },
 };
 
 export default function RootLayout({
